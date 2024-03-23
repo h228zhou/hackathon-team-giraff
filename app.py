1eabbbcbdb43da90204f673b537b4b48c205d265
h228zhou <ryanzed12138@gmail.com>
2024-03-23 16:26:10 -0500

    Updated Front end

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, redirect, url_for
+import json 
+import sqlite3
+import os
+import uuid
 
 myapp = Flask(__name__)
 
+game_data = {}
+
 @myapp.route('/')
 def index():
+    return render_template('home.html')
+
+@myapp.route('/setup')
+def game_setup():
+    game_data.clear()
     return render_template('trpg_setup.html')
 
 @myapp.route('/submit', methods=['POST'])
@@ -23,17 +34,21 @@ def submit():
         player_phone = request.form.get(player_phone_key, '')
         players_info.append({'name': player_name, 'phone': player_phone})
     
+    game_data['topic'] = topic
+    game_data['num_players'] = num_players
+    game_data['players_info'] = players_info
     # Here, you can process the data or store it as needed
     print("Topic:", topic)
     print("Number of Players:", num_players)
     print("Players Info:", players_info)
     
     # Just returning the data for demonstration; in a real app, you might redirect or return a success response
-    return jsonify({
-        'topic': topic,
-        'num_players': num_players,
-        'players_info': players_info
-    })
+    return redirect(url_for('show_game'))
+
+@myapp.route('/game', methods=['GET'])
+def show_game():
+    # Todo: Check if the game is still in progress
+    return render_template('game_start.html', game_setup=game_data)
 
 if __name__ == '__main__':
     myapp.run(debug=True)
